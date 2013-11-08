269cd0c36c60370e62c0e9442cacbef22b70dc2e
Vibin Nair <vibin.nair@in.v2solutions.com>
2013-11-08 11:59:58 +0530

    Extended Project creation feature to check for page title.

--- features/step_definitions/project_step_definitions.rb
+++ features/step_definitions/project_step_definitions.rb
@@ -7,7 +7,7 @@ When /^I follow "New Project"$/ do
 end
 
 When /^I fill in "Name" with "TextMate 2"$/ do
-	fill_in('Name', with:'TextMate2') 
+	fill_in('Name', with:'TextMate 2') 
 end
 
 When /^I press "Create Project"$/ do
@@ -16,4 +16,18 @@ end
 
 Then /^I should see "Project has been created."$/ do
 	expect(page).to have_content 'Project has been created.'
+end
+
+Then /^I should be on the project page for "TextMate 2"$/ do
+	# We use the exclamatory find_by_name! function as it throws an exception when the specific row 
+	# is not found in table. 	
+
+	# Rails creates dynamic find_by_* methods for searching, where * will be a columnname of the table
+    # to which the Model corresponds.
+	project = Project.find_by_name!("TextMate 2")
+	project_path(project)
+end
+
+Then /^I should see "TextMate2 - Projects - Ticketee"$/ do
+	expect(page).to have_title("TextMate2 - Projects - Ticketee")
 end
