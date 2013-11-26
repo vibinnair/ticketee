b9b229a5c455aee8803e01b1846f8be5a6d17ab7
Vibin Nair <vibin.nair@in.v2solutions.com>
2013-11-26 17:08:41 +0530

    Implemented edit and update tickets functionality.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,7 +2,9 @@ class TicketsController < ApplicationController
 	before_filter :find_project # no options are required as in case of ticket this method has to 
 								# run before all the actions in TicketController.  
 
-	before_filter :find_ticket, :only => [:show]							
+	before_filter :find_ticket, :only => [	:show, 
+										  	:edit,
+											:update]
 
 	def new
 		@ticket = @project.tickets.build
@@ -12,7 +14,7 @@ class TicketsController < ApplicationController
 		@ticket = @project.tickets.build(params[:ticket])
 		if @ticket.save
 			flash[:notice] = "Ticket has been created."
-			redirect_to [@project, @ticket] 
+			redirect_to [@project, @ticket]  # => same as calling redirect_to project_ticket_path(@project, @ticket)
 		else
 			flash[:alert] = "Ticket has not been created."
 			render :action => "new"
@@ -20,6 +22,20 @@ class TicketsController < ApplicationController
 	end
 
 	def show
+	end
+
+	def edit 
+	end
+
+	def update
+		# @ticket and @project are already being obtained in the before_filter, so no need to find them.
+		if @ticket.update_attributes(params[:ticket])
+			flash[:notice] = "Ticket has been updated."
+			redirect_to project_ticket_path(@project, @ticket)
+		else
+			flash[:alert] = "Ticket has not been updated."
+			render :action => "edit"
+		end
 	end
 
 	private 
